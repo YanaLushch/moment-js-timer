66f74e13e16c6371e48fc882e811584a6917d731
Yana <49374873+YanaLushch@users.noreply.github.com>
2021-07-26 16:09:36 +0300

    added husky, eslint, prettier

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,45 @@
-var gulp = require("gulp");
-var browserify = require("browserify");
-var source = require('vinyl-source-stream');
-var watchify = require("watchify");
-var uglify = require('gulp-uglify');
-var tsify = require("tsify");
-var sourcemaps = require('gulp-sourcemaps');
-var buffer = require('vinyl-buffer');
-var gutil = require("gulp-util");
-var paths = {
-    pages: ['src/*.html']
+const gulp = require('gulp');
+const browserify = require('browserify');
+const source = require('vinyl-source-stream');
+const watchify = require('watchify');
+const uglify = require('gulp-uglify');
+const tsify = require('tsify');
+const sourcemaps = require('gulp-sourcemaps');
+const buffer = require('vinyl-buffer');
+const gutil = require('gulp-util');
+
+const paths = {
+  pages: ['src/*.html'],
 };
 
-var watchedBrowserify = watchify(browserify({
+const watchedBrowserify = watchify(
+  browserify({
     basedir: '.',
-    debug: true,  
+    debug: true,
     entries: ['src/main.ts'],
     cache: {},
-    packageCache: {}
-}).plugin(tsify));
+    packageCache: {},
+  }).plugin(tsify)
+);
 
-gulp.task('copy-html', function () {
-    return gulp.src(paths.pages)
-        .pipe(gulp.dest("dist"));
-});
+gulp.task('copy-html', () => gulp.src(paths.pages).pipe(gulp.dest('dist')));
 
 function babelifyAndBundle() {
-    return watchedBrowserify
-        .transform('babelify', {
-            presets: ['es2015'],
-            extensions: ['.ts']
-         })
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest("dist"));
-};
+  return watchedBrowserify
+    .transform('babelify', {
+      presets: ['es2015'],
+      extensions: ['.ts'],
+    })
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist'));
+}
 
 gulp.task('default', gulp.series('copy-html', babelifyAndBundle));
 
-watchedBrowserify.on("update", babelifyAndBundle);
-watchedBrowserify.on("log", gutil.log);
+watchedBrowserify.on('update', babelifyAndBundle);
+watchedBrowserify.on('log', gutil.log);
